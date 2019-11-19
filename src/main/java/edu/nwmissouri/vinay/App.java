a770341e85f0cb38afc38ff733b8a329f09f7f9f
vinaylinginedi <vinaylinginedi@yahoo.in>
2019-11-19 13:43:46 -0600

    importing files

---
+++ src/main/java/edu/nwmissouri/vinay/App.java
@@ -0,0 +1,26 @@
+package edu.nwmissouri.vinay;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+import java.util.Arrays;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.Comparator;
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
